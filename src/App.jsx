9d7e2ca4a6b4ccd8d277de768380f079abaa78f3
maundu55 <marymaundu.mm@gmail.com>
2025-02-21 13:03:46 +0300

    update Loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react"
 import { LoadingScreen } from "./components/LoadingScreen"
-import Home from './components/sections/Home'
 
 const App = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
   return (
-    <div>
-      <LoadingScreen />
+    <>
+    {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+    <div
+      className={`min-h-screen transition-opacity duration-700 ${
+        isLoaded ? "opacity-100" : "opacity-0"
+      } bg-black text-gray-100`}
+    >
+      {/* <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <Home />
+      <About />
+      <Projects />
+      <Contact /> */}
     </div>
+  </>
   )
 }
 
