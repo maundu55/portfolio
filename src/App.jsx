3c22c21979ff297a8c3ff4f0d6641595d38cf28d
maundu55 <marymaundu.mm@gmail.com>
2025-02-25 15:01:37 +0300

    add About section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { LoadingScreen } from "./components/LoadingScreen"
 import Navbar from "./components/Navbar";
 import MobileMenu from "./components/MobileMenu";
 import Home from "./components/sections/Home";
+import About from './components/sections/About'
 
 const App = () => {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -19,8 +20,8 @@ const App = () => {
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <Home />
-      {/* <About />
-      <Projects />
+      <About />
+      {/* <Projects />
       <Contact /> */}
     </div>
   </>
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <section id='home' className='min-h-screen flex items-center justify-center relative'>
       <div className='text-center z-10 px-4'>
-        <h1 className='text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right'>Hi, I'm Mary Maundu</h1>
+        <h1 className='text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right'>Hi, I'm Mary Maundu</h1>
         <p className='text-gray-400 text-lg mb-8 max-w-lg mx-auto'>
           I'm a Software developer who loves crafting clean, scalable web applications. My goal is to build solutions that offer both exceptional perfomance and a delightful user experience.
         </p>
