9a9717f2f497d87d537935cf4d54bb7ae6bdb8b2
maundu55 <marymaundu.mm@gmail.com>
2025-02-26 15:26:33 +0300

    add contact

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MobileMenu from "./components/MobileMenu";
 import Home from "./components/sections/Home";
 import About from './components/sections/About';
 import Projects from './components/sections/Projects'
+import Contact from "./components/sections/Contact";
 
 const App = () => {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -23,7 +24,7 @@ const App = () => {
       <Home />
       <About />
       <Projects />
-      {/* <Contact /> */}
+      <Contact />
     </div>
   </>
   )
