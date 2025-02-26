520c89e56ed6f47b3316553e6793cd700093f5a7
maundu55 <marymaundu.mm@gmail.com>
2025-02-26 17:05:29 +0300

    adjust width for form

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -28,7 +28,7 @@ const Contact = () => {
   return (
    <section id='contact' className='min-h-screen flex items-center justify-center py-20'>
       <RevealOnScroll>
-        <div className='px-4 w-150'>
+        <div className='px-4 w-full'>
             <h2 className='text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center' >Get In Touch</h2>
             <form className='space-y-6' onSubmit={handleSubmit}>
                 <div className='relative'>
