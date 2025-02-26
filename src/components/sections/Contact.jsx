55db280717093743c627a6f8bb61bbb9d8188666
maundu55 <marymaundu.mm@gmail.com>
2025-02-26 16:14:13 +0300

    add env.vite

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -14,10 +14,14 @@ const Contact = () => {
     const PUBLIC_KEY ='QmZDMZ_UZPZmtKz7X'
 
     const handleSubmit =(e)=>{
-        emailjs.preventDefault()
+        e.preventDefault()
 
-        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY).then((result)=>{
-            alert('Message Sent!')
+        emailjs.sendForm( import.meta.env.VITE_SERVICE_ID,
+            import.meta.env.VITE_TEMPLATE_ID,
+            e.target,
+            import.meta.env.VITE_PUBLIC_KEY).then((result)=>{
+            alert('Message Sent!');
+            setFormData({name:'', email:'', message:''})
         }).catch(()=> alert('Oops! Something went wrong!'))
     }
 
@@ -26,15 +30,15 @@ const Contact = () => {
       <RevealOnScroll>
         <div className='px-4 w-150'>
             <h2 className='text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center' >Get In Touch</h2>
-            <form className='space-y-6'>
+            <form className='space-y-6' onSubmit={handleSubmit}>
                 <div className='relative'>
-                    <input type="text" id='name' name='name' required className='w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5' placeholder='Name...'/>
+                    <input type="text" id='name' name='name' required value={formData.name} className='w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5' placeholder='Name...' onChange={(e)=> setFormData({...formData, name:e.target.value})} />
                 </div>
                 <div className='relative'>
-                    <input type="email" id='email' name='email' required className='w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5' placeholder='exaample@gmail.com'/>
+                    <input type="email" id='email' name='email' required value={formData.email} className='w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5' placeholder='exaample@gmail.com' onChange={(e)=> setFormData({...formData, email:e.target.value})}/>
                 </div>
                 <div className='relative'>
-                    <textarea id='message' name='message' required rows={5} className='w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5' placeholder='Message...'/>
+                    <textarea id='message' name='message' required value={formData.message} rows={5} className='w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5' placeholder='Message...' onChange={(e)=> setFormData({...formData, message:e.target.value})}/>
                 </div>
                 <button type='submit' className='w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transiton relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]'>
                     Send Message
